eaad8f2c1e877a87e366646a6c95e5314f45249c
Stanley Shikwambi <shikwambipolly@gmail.com>
2024-12-11 10:48:31 +0200

    set up

---
+++ src/main/java/IJG/Portal/PricingPortalApplication.java
@@ -0,0 +1,13 @@
+package IJG.Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IJG/Portal/PricingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package IJG.Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
